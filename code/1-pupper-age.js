607e3e21790eecf14c0a6ae49c0a3ceeebf961d0
sophiep07 <parsonso14@craighead.school.nz>
2023-05-31 15:03:39 +1200

    did the dog age and changed color to green

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -6,4 +6,8 @@ function pupperAge() {
   alert('Dog years are human years times seven.')
 
   let humanYears = prompt('How old is your dog in human years?')
+
+  let dogAge = humanYears * 7
+
+  alert('Your dog is ' + dogAge + ' years old.')
 }
