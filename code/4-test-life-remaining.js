b69d0d0307f7300ceeb19d3fd1d890582afe572d
sophiep07 <parsonso14@craighead.school.nz>
2023-06-01 10:14:41 +1200

    did the test life remaining

--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,8 @@
 // ####################################
 
 function lifeRemaining() {
-  
+    alert('The average life expectancy is 83.5 years.')
+    let originalAge = prompt('How old are you?')
+    let timeLeft = 83.5 - originalAge
+    alert("You've got " + timeLeft + " years left in you.")
 }
