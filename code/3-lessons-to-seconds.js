201195f14a579bd80bca4f61e40d3498fa65ade4
sophiep07 <parsonso14@craighead.school.nz>
2023-06-01 10:11:53 +1200

    did lessons to seconds and changed the color to green

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,7 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+  let lessonLeft = prompt('How many lessons do you have left today?')
+  let timeLeft = lessonLeft * 3600
+  alert('You have ' + timeLeft + ' seconds left in school today.')
 }
